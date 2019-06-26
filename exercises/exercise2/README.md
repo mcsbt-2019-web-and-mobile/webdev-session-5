256a96c02ecd7d4fde74f104e9429503ea7b8920
Pepe <pepe@pepegar.com>
2019-06-27 01:07:47 +0200

    add exercise2
    solved

---
+++ exercises/exercise2/README.md
@@ -0,0 +1,4 @@
+# exercise2
+
+Create a new route for rendering a user page.  Make sure you can use
+this same route for all users.
---
+++ exercises/exercise2/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './index.css';
+
+const User = (props) => (
+  <div id="user">
+    <h2>page for user: {props.match.params.username}</h2>
+  </div>
+);
+
+const Home = (props) => (
+  <h1>this is the home</h1>
+);
+
+const App = () => (
+  <div>
+    <Route exact path="/" component={Home}/>
+    <Route path="/user/:username" component={User}/>
+  </div>
+);
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
